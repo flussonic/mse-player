162fb0c98256d302a48daec366b2796faef4ba7a
otmjka <nindeec@gmail.com>
2018-08-29 17:18:05 +0500

    update lib version; update lib buildings

--- dist/FlussonicMsePlayer.js
+++ dist/FlussonicMsePlayer.js
@@ -660,7 +660,7 @@ var MSEPlayer = function () {
   _createClass(MSEPlayer, null, [{
     key: 'version',
     get: function get() {
-      return "18.08.4";
+      return "18.08.5";
     }
   }]);
 
