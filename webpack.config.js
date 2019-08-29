2d8d1e17a3e71b090b6f8dbdd7d51e724ef30a4a
Alexander Chaika <a.chayka@erlyvideo.org>
2019-08-29 17:48:33 +0300

    explicit production mode, webpack configs #9229

--- webpack.config.js
+++ webpack.config.js
@@ -21,41 +21,6 @@ if (process.env.npm_lifecycle_event === 'build-min') {
     'FlussonicMsePlayer.min': [path.resolve(__dirname, 'src/FlussonicMsePlayer.js')],
   }
   webpackConfig.plugins.push(new webpack.LoaderOptionsPlugin({minimize: true, debug: false}))
-  // webpackConfig.plugins.push(
-  //   new UglifyJsPlugin({
-  //     include: /\/includes/,
-  //     cache: true,
-  //     parallel: true,
-  //     uglifyOptions: {
-  //       ecma: 6,
-  //       mangle: true,
-  //       compress: {
-  //         // Disabled because of an issue with Uglify breaking seemingly valid code:
-  //         // https://github.com/facebookincubator/create-react-app/issues/2376
-  //         // Pending further investigation:
-  //         // https://github.com/mishoo/UglifyJS2/issues/2011
-  //         comparisons: false,
-  //         pure_getters: true,
-  //         unused: true,
-  //         dead_code: true, // big one--strip code that will never execute
-  //         warnings: false, // good for prod apps so users can't peek behind curtain
-  //         drop_debugger: true,
-  //         conditionals: true,
-  //         evaluate: true,
-  //         drop_console: true, // strips console statements
-  //         sequences: true,
-  //         booleans: true,
-  //       },
-  //     },
-  //     // output: {
-  //     //   comments: false,
-  //     //   // Turned on because emoji and regex is not minified properly using default
-  //     //   // https://github.com/facebookincubator/create-react-app/issues/2488
-  //     //   ascii_only: true,
-  //     // },
-  //     sourceMap: false,
-  //   })
-  // )
 } else if (process.env.npm_lifecycle_event === 'build') {
   webpackConfig.plugins.push(new webpack.LoaderOptionsPlugin({minimize: false, debug: false}))
 } else if (process.env.npm_lifecycle_event !== 'start') {
